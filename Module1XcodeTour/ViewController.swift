c90ebd686179c467d0f4fe7297fc112e17d81f21
Charles Lucas Pedigo <charles_pedigo@mymail.eku.edu>
2026-01-22 12:22:11 -0500

    1/22/2026

--- Module1XcodeTour/ViewController.swift
+++ Module1XcodeTour/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var valueLabel: UILabel!
+    @IBOutlet weak var inputTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func resultButton(_ sender: UIButton) {
+        
+        
+    }
+    
 
 }
 
